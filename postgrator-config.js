36961cc5fd0a18ee0cdc5fb73f85fae4cbeaba9b
Tucker <gillitux@gmail.com>
2021-02-03 11:23:27 -0500

    reconfigure postgrator-config

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl =
+  process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false;
 
 module.exports = {
   migrationDirectory: 'migrations',
