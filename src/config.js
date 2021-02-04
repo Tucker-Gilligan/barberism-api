f5ee49be62bd7e1408bab3af7cc6a17f7ce28e3e
Tucker <gillitux@gmail.com>
2021-02-04 17:10:52 -0500

    changed localhost 8080 from 8000

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL || 'postgresql://gilligan@localhost/barberism',
---
+++ src/store.js
@@ -0,0 +1,13 @@
+const barbers = [
+  {
+    id: 1,
+    barber_name: 'Joe Barber',
+    barber_location: 'New Jersey',
+    website_url: 'http://www.google.com/',
+    description: 'google homepage',
+    phone: '000-000-0000',
+    email: 'joe-barber@gmail.com',
+  },
+];
+
+module.exports = { barbers };
