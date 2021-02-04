9795cbf41b03aded80f0b34d176c47bc445fa08b
Tucker <gillitux@gmail.com>
2021-02-04 13:18:52 -0500

    removed commented-out code and unused files

--- src/app.js
+++ src/app.js
@@ -4,11 +4,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-// const logger = require('./logger');
 const errorHandler = require('./errorHandler');
 const { v4: uuid } = require('uuid');
 const barberRouter = require('./barbers/barber-router');
-// const validateBearerToken = require('./validateBearerToken');
 
 const app = express();
 const bodyParser = express.json();
@@ -16,7 +14,6 @@ const bodyParser = express.json();
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(cors());
-// app.use(validateBearerToken);
 app.use(morgan(morganOption));
 app.use(helmet());
 
